18107136b4133828c16cc1e792a8e0550be429c0
Asafe Orneles <asafeorneles@gmail.com>
2026-02-20 18:46:50 -0300

    Spring Boot 2 Essentials 07

--- src/main/java/com/example/springboot2_essentials/controller/AnimeController.java
+++ src/main/java/com/example/springboot2_essentials/controller/AnimeController.java
@@ -4,6 +4,7 @@ import com.example.springboot2_essentials.domain.Anime;
 import com.example.springboot2_essentials.util.DataUtil;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,26 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequestMapping(path = "anime")
+@Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
 
     private final DataUtil dataUtil;
 
-    //    localhost:8080/anime/list
-    //    @RequestMapping(method = RequestMethod.GET, path = "list") Antes se usava essa configuração, agora fica mais fácil com o @GetMapping...
     @GetMapping(path = "list")
-    public List<Anime> list(){
+    public List<Anime> list() {
         log.info(dataUtil.formatterDateTimeDataBaseStyle(LocalDateTime.now()));
         return List.of(new Anime("Hunter x Hunter"), new Anime("Berserk"));
     }
 
-    @GetMapping(path = "list2")
-    public List<Anime> list2(){
-        log.info(dataUtil.formatterDateTimeDataBaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Hunter x Hunter"), new Anime("Berserk"), new Anime("One Piece"));
-    }
-
 }
+
