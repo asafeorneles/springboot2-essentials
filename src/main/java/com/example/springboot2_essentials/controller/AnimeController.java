e15c4b8754ba025dad4cc705768844511c8714e3
Asafe Orneles <asafeorneles@gmail.com>
2026-02-24 19:19:38 -0300

    Spring Boot 2 Essentials 08

--- src/main/java/com/example/springboot2_essentials/controller/AnimeController.java
+++ src/main/java/com/example/springboot2_essentials/controller/AnimeController.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,15 @@ public class AnimeController {
     private final DataUtil dataUtil;
     private final AnimeService animeService;
 
-    @GetMapping(path = "list")
-    public List<Anime> list() {
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
         log.info(dataUtil.formatterDateTimeDataBaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 
 }
--- src/main/java/com/example/springboot2_essentials/service/AnimeService.java
+++ src/main/java/com/example/springboot2_essentials/service/AnimeService.java
@@ -1,14 +1,25 @@
 package com.example.springboot2_essentials.service;
 
 import com.example.springboot2_essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
     // private final AnimeRepository animeRepository;
+    List<Anime> animes = List.of(new Anime(1L, "Hunter x Hunter"), new Anime(2L,"Berserk"));
+
     public List<Anime> listAll(){
-        return List.of(new Anime(1L, "Hunter x Hunter"), new Anime(2L,"Berserk"));
+        return animes;
+    }
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found."));
     }
 }
