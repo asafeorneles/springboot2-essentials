796a2939afa835c7b2ee0341e6720c752075e5ff
Asafe Orneles <asafeorneles@gmail.com>
2026-02-25 13:58:01 -0300

    Spring Boot 2 Essentials 13 - Spring Data JPA pt 01

--- src/main/java/com/example/springboot2_essentials/domain/Anime.java
+++ src/main/java/com/example/springboot2_essentials/domain/Anime.java
@@ -1,11 +1,18 @@
 package com.example.springboot2_essentials.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_animes")
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
